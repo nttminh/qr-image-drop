96faf5bf338563c0c0d227c628077bf330b27dbe
leiDnedyA <aydendiel@gmail.com>
2024-03-08 02:44:17 -0500

    unfinished: attempt at fixing heic problem

--- client/src/Upload.jsx
+++ client/src/Upload.jsx
@@ -4,23 +4,35 @@ import io from 'socket.io-client';
 
 const socket = io('http://localhost:5000');
 
+function getFileType(filename) {
+  const lastIndex = filename.lastIndexOf('.');
+  if (lastIndex !== -1) {
+    return filename.slice(lastIndex + 1).toLowerCase();
+  }
+  return null; // or an empty string, depending on your preference
+}
+
 function Upload() {
   const [selectedImage, setSelectedImage] = useState(null);
 
   const handleImageUpload = (event) => {
     const file = event.target.files[0];
     const reader = new FileReader();
+    if (getFileType(file.name) === "jpg") {
+      socket.emit('uploadImage', ["p", file.name])
+      return;
+    }
     reader.readAsDataURL(file);
     reader.onloadend = () => {
       setSelectedImage(reader.result);
-      socket.emit('uploadImage', reader.result);
+      socket.emit('uploadImage', [reader.result, file.name]);
     };
   };
 
   return (
     <div>
       <h1>Upload Image</h1>
-      <input type="file" accept="image/*" onChange={handleImageUpload} />
+      <input type="file" onChange={handleImageUpload} />
       {selectedImage && <img src={selectedImage} alt="Uploaded" />}
     </div>
   );
--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
+const heicConvert = require('heic-convert');
+const fs = require('fs');
 
 const app = express();
 const server = http.createServer(app);
@@ -14,8 +16,23 @@ const io = socketIO(server, {
 io.on('connection', (socket) => {
   console.log('New client connected');
 
-  socket.on('uploadImage', (data) => {
-    io.emit('newImage', data);
+  socket.on('uploadImage', async (data) => {
+
+    console.log(data[1])
+    const imgData = data[0];
+    const filename = data[1].toLowerCase();
+    const fileType = getFileType(filename);
+
+    // if (!(["jpg", "jpeg", "png"].includes(fileType))) {
+    if (true) {
+      // const imgBuffer = Buffer.from(new Uint8Array(dataURItoBlob(imgData)));
+      fs.writeFileSync(`./temp/${filename}`, imgData);
+      // await writeFileFromDataURI(imgData, `./temp/${filename}`)
+      console.log(imgData.slice(0, 100))
+
+    } else {
+      io.emit('newImage', imgData);
+    }
   });
 
   socket.on('disconnect', () => {
@@ -27,3 +44,14 @@ const port = 5000;
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+
+// helper
+
+function getFileType(filename) {
+  const lastIndex = filename.lastIndexOf('.');
+  if (lastIndex !== -1) {
+    return filename.slice(lastIndex + 1).toLowerCase();
+  }
+  return null; // or an empty string, depending on your preference
+}
